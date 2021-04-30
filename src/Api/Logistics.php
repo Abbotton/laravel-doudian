bc37f865fd841cad5acbdbb5e690d24e1f1e75ca
Abbotton <uctoo@foxmail.com>
2021-04-30 08:24:13 +0800

    新增电子免单相关接口

--- src/Api/Logistics.php
+++ src/Api/Logistics.php
@@ -121,4 +121,107 @@ class Logistics extends BaseRequest
     {
         return $this->httpPost('order/logisticsEditByPack', $params);
     }
+
+    /**
+     * 根据省获取全量四级地址.
+     *
+     * @param array $params
+     * @return array
+     * @throws InvalidArgumentException
+     * @throws RequestException
+     */
+    public function getAreasByProvince(array $params): array
+    {
+        return $this->httpPost('address/getAreasByProvince', $params);
+    }
+
+    /**
+     * 获取四级地址全量省份信息.
+     *
+     * @return array
+     * @throws InvalidArgumentException
+     * @throws RequestException
+     */
+    public function getProvince(): array
+    {
+        return $this->httpPost('address/getProvince');
+    }
+
+    /**
+     * 顺丰新下单接口.
+     *
+     * @param array $params
+     * @return array
+     * @throws InvalidArgumentException
+     * @throws RequestException
+     */
+    public function createSFOrder(array $params): array
+    {
+        return $this->httpPost('logistics/createSFOrder', $params);
+    }
+
+    /**
+     * 用于ISV/商家ERP系统 端发起取消已获取的电子面单号.
+     *
+     * @param array $params
+     * @return array
+     * @throws InvalidArgumentException
+     * @throws RequestException
+     */
+    public function cancelOrder(array $params): array
+    {
+        return $this->httpPost('logistics/cancelOrder', $params);
+    }
+
+    /**
+     * 查询地址快递是否可以送达.
+     *
+     * @param array $params
+     * @return array
+     * @throws InvalidArgumentException
+     * @throws RequestException
+     */
+    public function getOutRange(array $params): array
+    {
+        return $this->httpPost('logistics/getOutRange', $params);
+    }
+
+    /**
+     * 查询商家和物流商的订购关系以及物流单号使用情况.
+     *
+     * @param array $params
+     * @return array
+     * @throws InvalidArgumentException
+     * @throws RequestException
+     */
+    public function listShopNetsite(array $params): array
+    {
+        return $this->httpPost('logistics/listShopNetsite', $params);
+    }
+
+    /**
+     * 商家ERP/ISV 向字节电子面单系统获取单号和打印信息.
+     *
+     * @param array $params
+     * @return array
+     * @throws InvalidArgumentException
+     * @throws RequestException
+     */
+    public function newCreateOrder(array $params): array
+    {
+        return $this->httpPost('logistics/newCreateOrder', $params);
+    }
+
+    /**
+     * 更新收件人信息 以及发件人名字联系方式信息，不支持顺丰速递面单信息更新.
+     *
+     * @param array $params
+     * @return array
+     * @throws InvalidArgumentException
+     * @throws RequestException
+     */
+    public function updateOrder(array $params): array
+    {
+        return $this->httpPost('logistics/updateOrder', $params);
+    }
 }
